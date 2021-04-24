af1687d2b65d36f4c2bcaebad5e2f6ab87c9fe5c
Arkadiy <chekryzhov2000@mail.ru>
2021-04-24 23:28:27 +0300

    auth

--- app/src/main/java/org/ruiners/dotastatistics/MainActivity.java
+++ app/src/main/java/org/ruiners/dotastatistics/MainActivity.java
@@ -2,6 +2,7 @@ package org.ruiners.dotastatistics;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -12,9 +13,13 @@ import org.ruiners.dotastatistics.repository.MatchRepositoryImpl;
 public class MainActivity extends AppCompatActivity implements ProfilePresenter.View {
 
     private ProfilePresenter mProfilePresenter;
+    private SignInActivity mSignInActivity;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Intent intent = new Intent(this, SignInActivity.class);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         View button = findViewById(R.id.getMatch);
@@ -25,12 +30,21 @@ public class MainActivity extends AppCompatActivity implements ProfilePresenter.
                 mProfilePresenter.onShowMatchClicked();
             }
         });
+        View buttonSignIn = findViewById(R.id.signin);
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
     }
 
     public void showMatchId(String id) {
         TextView match = (TextView)findViewById(R.id.matchId);
         match.setText(id);
     }
+
 
     @Override
     protected void onDestroy() {
